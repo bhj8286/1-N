009cf68b0738d6b687c41975bd016f5d5862d257
bhj8286 <bjs219@naver.com>
2023-11-06 13:30:15 +0900

    Comment Create 기능 구현

--- articles/models.py
+++ articles/models.py
@@ -8,3 +8,4 @@ class Article(models.Model):
 class Comment(models.Model):
     content = models.TextField()
     article = models.ForeignKey(Article, on_delete=models.CASCADE)  #일반적으로 pk를 받아오려는 클래스와 이름 같게 설정, ForeignKey로 1:N 관계 설정
+    # article을 foreignkey로 설정하면 장고가 자동으로 article_id를 만들어줌
