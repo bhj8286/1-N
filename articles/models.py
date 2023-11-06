b4eb31faa9464b3bba7da078e74d11cb0b5de9c2
bhj8286 <bjs219@naver.com>
2023-11-06 09:42:01 +0900

    모델링 & 마이그레이션

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+
+class Comment(models.Model):
+    content = models.TextField()
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)  #일반적으로 pk를 받아오려는 클래스와 이름 같게 설정, ForeignKey로 1:N 관계 설정
